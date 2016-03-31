1d75ee9b0d10e272dce511ff8231ddf266295fe3
pupanya <pupanya@mail.ru>
2016-03-31 21:11:29 +0500

    styl and pic

--- dev/mock-players.ts
+++ dev/mock-players.ts
@@ -1,11 +1,70 @@
 import {Player} from './player';
 
 export var PLAYERS: Player[] = [
-    {"id": 1, "name": "Mr. Nice"},
-    {"id": 2, "name": "Narco"},
-    {"id": 3, "name": "Bombasto"},
-    {"id": 4, "name": "Celeritas"},
-    {"id": 5, "name": "Magneta"},
-    {"id": 6, "name": "RubberMan"},
-    {"id": 7, "name": "Dynama"}
+    {
+        "id": 1,
+        "name": "pupanya",
+        "firstname": "Шамиль",
+        "lastname": "Сагиров",
+        "birthdate": "",
+        "city": "Екатеринбург",
+        "imgpath": "",
+        "statlink": "pupik",
+        "steam": "id/pupik"
+    },
+    {
+        "id": 2,
+        "name": "cutman",
+        "firstname": "Александр",
+        "lastname": "Зайцев",
+        "birthdate": "",
+        "city": "Набережные Челны",
+        "imgpath": "",
+        "statlink": "zaytsevalek",
+        "steam": "id/zaytsevalek"
+    },
+    {
+        "id": 3,
+        "name": "Ubica_Padonkoff",
+        "firstname": "Алексей",
+        "lastname": "Ромашин",
+        "birthdate": "",
+        "city": "Казань",
+        "imgpath": "",
+        "statlink": "Ubica_Padonkoff",
+        "steam": "id/Ubica_Padonkoff"
+    },
+    {
+        "id": 4,
+        "name": "Webba",
+        "firstname": "Олег",
+        "lastname": "Соболев",
+        "birthdate": "",
+        "city": "Екатеринбург",
+        "imgpath": "",
+        "statlink": "webko",
+        "steam": "id/webko"
+    },
+    {
+        "id": 5,
+        "name": "rramsikk",
+        "firstname": "Александр",
+        "lastname": "Белик",
+        "birthdate": "",
+        "city": "Екатеринбург/Тюмень",
+        "imgpath": "",
+        "statlink": "76561198085219767",
+        "steam": "profiles/76561198085219767"
+    },
+    {
+        "id": 6,
+        "name": "Molotovich",
+        "firstname": "Вячеслав",
+        "lastname": "Литвин",
+        "birthdate": "",
+        "city": "Екатеринбург",
+        "imgpath": "",
+        "statlink": "76561198005944552",
+        "steam": "profiles/76561198005944552"
+    }
 ];
